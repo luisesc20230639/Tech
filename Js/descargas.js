5116c71bb778cbb3f38a95ac29a703f94030c914
Maxicast96 <chanakane820@gmail.com>
2023-09-01 15:42:15 -0600

    descargas y licencias

--- Js/descargas.js
+++ Js/descargas.js
@@ -2,44 +2,55 @@
 const selectOpcion = document.getElementById("selectorLicencia")
 
 
-const PrecioLicenciaSencilla = 150;
-const PrecioLicenciaPremium = 500;
-
 
 const selectorLicencia = document.getElementById('selectorLicencia');
 const descripcionLicencia = document.getElementById('descripcion');
 const comprarBtn = document.getElementById('comprarBtn');
 let precioLicencia = document.getElementById('precio');
 
+let licencia = 0;
 
 selectorLicencia.addEventListener('change', function(){
     
     if (selectorLicencia.value === 'sencilla') {
         descripcionLicencia.textContent = 'Licencia sencilla: Acceso a funciones básicas y funciones limitadas';
-        precioLicencia.textContent = `$${PrecioLicenciaSencilla}`
+        licencia=150;
+        precioLicencia.textContent = `$${licencia}`
     } else if (selectorLicencia.value == 'premium'){
         descripcionLicencia.textContent = 'Licencia Premium: Acceso completo a todas las funciones, seguridad avanzada y soporte técnico ampliado';
-        precioLicencia.textContent = ` $${PrecioLicenciaPremium}`;
+        licencia=450;
+        precioLicencia.textContent = ` $${ licencia}`;
+
     } else {
+        licencia=0;
         descripcionLicencia.textContent = 'Seleccione una Licencia';
         precioLicencia.textContent = `$`
     }
 })
 
 comprarBtn.addEventListener('click', function(){
-    
-    Swal.fire({
-        icon: 'question',
-        title: 'Confirmar compra',
-        text: '¿Estas seguro de comprar la licencia?',
-        showCancelButton: true,
-        confirmButtonText: 'Si, comprar',
-        cancelButtonText: 'Cancelar',
 
-    }).then((result) => {
-        
-        if(result.isConfirmed){
-            swal.fire('¡Licencia comprada exitosamente!', '', 'success')
-        }
-    })
+    if (licencia===0) {
+        swal.fire('¡Licencia no se puede comprar comprada exitosamente!', '', 'error')
+        return
+    }
+    else{
+
+        Swal.fire({
+            icon: 'question',
+            title: 'Confirmar compra',
+            text: '¿Estas seguro de comprar la licencia?',
+            showCancelButton: true,
+            confirmButtonText: 'Si, comprar',
+            cancelButtonText: 'Cancelar',
+    
+        }).then((result) => {
+            
+            if(result.isConfirmed){
+                swal.fire('¡Licencia comprada exitosamente!', '', 'success')
+            }
+        })
+    }
+
+
 })
